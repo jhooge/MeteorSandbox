9e071751732d91e397d92ac9129e1a85e70af39c
Jens Hooge <jhooge@iMac.fritz.box>
2016-01-19 01:00:19 +0100

    Added Meteor+React+D3 Example

--- d3-dashboard/d3-dashboard.jsx
+++
@@ -1,9 +0,0 @@
-Data = new Mongo.Collection("BreastCancer");
-//Data = Mongo.Collection.get("BreastCancer");
-if (Meteor.isClient) {
-
-  Meteor.startup(function() {
-//    console.log(Data.find());
-    React.render(<App />, document.getElementById("render-target"));
-  });
-}
